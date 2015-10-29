981400ce390e1af79b1d425c260b15cb7db7ab9e
Oussama LAKLALECH <olaklalech@sqli.com>
2015-10-29 11:31:48 +0100

    ajout de la console h2

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 Done : Premier Web Service appel√© (GET:/greeting)
 Done : Mise en place de SPRING JPA
 Done : Mise en place d'un premier Test (JUNIT)
+DONE : Ajout de H2 comme BDD In-memory, pour la consulter, il faut faire localhost:8080/console
 TODO : Int√grer une BDD
 TODO : d√velopper davantage de services (POST, GET, DELETE, PUT) 
 TODO : Int√grer les outils de test
--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -1,9 +1,13 @@
 package com.example;
 
+import org.h2.Driver;
+import org.h2.server.web.WebServlet;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -13,5 +17,12 @@ public class DemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
-   
+    
+    @Bean
+    ServletRegistrationBean h2servletRegistration(){
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean( new WebServlet());
+        registrationBean.addUrlMappings("/console/*");
+        return registrationBean;
+    }
+
 }
